aaf542e24645339aa034f590067fbdc0765a8dfb
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-28 14:47:10 +0800

    Updated test.

--- lqual-test.js
+++ lqual-test.js
@@ -2,20 +2,20 @@
 const assert = require( "assert" );
 const lqual = require( "./lqual.js" );
 
-assert.equal( lqual( 0, 0 ), true, "should return true" );
+assert.equal( lqual( 0, 0 ), true, "should be equal to true" );
 
-assert.equal( lqual( null, null ), true, "should return true" );
+assert.equal( lqual( null, null ), true, "should be equal to true" );
 
-assert.equal( lqual( undefined, undefined ), true, "should return true" );
+assert.equal( lqual( undefined, undefined ), true, "should be equal to true" );
 
-assert.equal( lqual( { }, { } ), true, "should return true" );
+assert.equal( lqual( { }, { } ), true, "should be equal to true" );
 
-assert.equal( lqual( global, global ), true, "should return true" );
+assert.equal( lqual( global, global ), true, "should be equal to true" );
 
-assert.equal( lqual( true, true ), true, "should return true" );
+assert.equal( lqual( true, true ), true, "should be equal to true" );
 
-assert.equal( lqual( true, false ), false, "should return false" );
+assert.equal( lqual( true, false ), false, "should be equal to false" );
 
-assert.equal( lqual( [ 1, 2, 3, 4, 5 ], [ 1, 2, 3, 4, 5 ] ), true, "should return true" );
+assert.equal( lqual( [ 1, 2, 3, 4, 5 ], [ 1, 2, 3, 4, 5 ] ), true, "should be equal to true" );
 
 console.log( "ok" );
